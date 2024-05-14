096f53722579710924fa96ca3a44f307e6932944
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-14 18:40:26 +0300

    Remove web components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import {Serianilla} from "../framework/Serianilla.js";
 import {App} from "./App.js";
-import Tab from "./web-components/Tab.js";
-
-customElements.define('app-tab', Tab);
 
 const root = document.getElementById('root');
 Serianilla.render(root, App);
