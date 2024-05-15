b69fdad26c73ca3fe8872fbb0f81f377e7369ac7
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-15 20:46:13 +0300

    Add attacher

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ export const App = () => {
     const imports = {Auth};
 
     const template = `
-    <div>
-        <$if true={hello}>
-            <Auth/>   
+    <div number={number}>
+        <Auth onClick={handle} prop={a}/>
+        <$if true={isComing}>
+            <div>Let's go!</div>
         </$if>
     </div>`;
 
-    const attach = {};
+    const attach = {
+        a: 'hello',
+        number: 12,
+        handle: e => console.log('Hello!'),
+        isComing: 12 + 2 === 15
+    };
 
     return {imports, template, attach};
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {App} from "./App.js";
 import {tokenize} from "../framework/internal/tokenizer.js";
-import {parseComponentChild} from "../framework/internal/component-parser.js";
+import {parseComponentChild} from "../framework/internal/parser.js";
+import {applyComponentAttachments} from "../framework/internal/attacher.js";
 
 // const root = document.getElementById('root');
 // Serianilla.render(root, App);
@@ -9,5 +10,10 @@ const {imports, template, attach} = App();
 const tokens = tokenize(template);
 console.log('tokens:', tokens);
 
-const ast = parseComponentChild(tokens)
+const ast = parseComponentChild(tokens);
 console.log('ast:', ast);
+
+const attachMap = attach ? new Map(Object.entries(attach)) : new Map();
+
+applyComponentAttachments(ast, attachMap, null);
+console.log('ast after attach:', ast);
