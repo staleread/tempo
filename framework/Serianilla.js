a2da00dce0076267fa995ffd7fb3a0e757aa2540
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-16 10:07:11 +0300

    Update Serianilla to fit new unwrapping approach and state management

--- framework/Serianilla.js
+++ framework/Serianilla.js
@@ -1,60 +1,21 @@
 import {renderAST, renderDiff} from "./internal/renderer.js";
-import {parseComponent} from "./internal/parser.js";
 import {refreshEvents} from "./internal/events.js";
+import {StateManager} from "./internal/state-manager.js";
+import {unwrapComponentTree} from "./internal/unwrapper.js";
+import {findUsedEvents} from "./internal/traversers.js";
 
 export const Serianilla = (function () {
     let _virtualDOM;
     let _rootComponent;
-    let _stateIndex = -1;
-    let _hooks = [];
-    let _localStateIndex = 0;
     let _eventSet;
     let _stateTimeout = null;
-
-    const _updateHooks = (tag, nesting) => {
-        _localStateIndex = 0;
-        _stateIndex++;
-
-        const current = _hooks[_stateIndex];
-
-        if (!current) {
-            _hooks[_stateIndex] = {tag, nesting, states: []};
-            return;
-        }
-
-        if (current.tag === tag && current.nesting === nesting) {
-            return;
-        }
-
-        if (current.tag !== tag && current.nesting >= nesting) {
-            let i = _stateIndex + 1;
-            let hook = _hooks[i];
-
-            while (hook && hook.nesting > nesting) {
-                hook = _hooks[++i];
-            }
-
-            _hooks.splice(_stateIndex, i - _stateIndex);
-
-            if (hook) {
-                return;
-            }
-
-            _hooks[_stateIndex] = {tag, nesting, states: []};
-            return;
-        }
-
-        if (current.nesting < nesting) {
-            _hooks.splice(_stateIndex - 1, 0, {tag, nesting, states: []});
-        }
-    }
+    let _stateManager;
 
     const _updateVirtualDOM = () => {
-        _stateIndex = -1;
+        _stateManager.reset();
 
-        const componentInfo = parseComponent(_rootComponent(), 0, _updateHooks);
-        const newEventSet = componentInfo.eventSet;
-        const node = componentInfo.ast;
+        const node = unwrapComponentTree(_rootComponent, _stateManager);
+        const newEventSet = findUsedEvents(node);
 
         const candidateDOM = {
             type: 'RootNode',
@@ -69,14 +30,13 @@ export const Serianilla = (function () {
 
     return {
         render(rootElement, rootComponent) {
-            _stateIndex = -1;
-            _hooks = [];
             _eventSet = new Set();
             _rootComponent = rootComponent;
 
-            const componentInfo = parseComponent(_rootComponent(), 0, _updateHooks);
-            const newEventSet = componentInfo.eventSet;
-            const node = componentInfo.ast;
+            _stateManager = new StateManager();
+
+            const node = unwrapComponentTree(_rootComponent, _stateManager);
+            const newEventSet = findUsedEvents(node);
 
             _virtualDOM = {
                 type: 'RootNode',
@@ -91,30 +51,30 @@ export const Serianilla = (function () {
         },
 
         useState(initialValue) {
-            const currentStates = _hooks[_stateIndex].states;
+            const states = _stateManager.currentBucket.states;
+            const index = _stateManager.currentStateIndex;
 
-            if (currentStates[_localStateIndex] === undefined) {
-                currentStates[_localStateIndex] = initialValue;
+            if (states[index] === undefined) {
+                states[index] = initialValue;
             }
-            const tmpStateIndex = _localStateIndex;
 
             const setValue = (newValue) => {
-                if (newValue === currentStates[tmpStateIndex]) {
+                if (newValue === states[index]) {
                     return;
                 }
-                currentStates[tmpStateIndex] = newValue;
+                states[index] = newValue;
 
                 if (_stateTimeout) {
                     return;
                 }
-
                 _stateTimeout = setTimeout(() => {
                     console.log(`It's time to update DOM`)
                     _updateVirtualDOM()
                     _stateTimeout = null;
                 }, 0);
             };
-            return [currentStates[_localStateIndex++], setValue];
+
+            return [states[index], setValue];
         }
     }
 })();
--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import {App} from "./App.js";
-import {tokenize} from "../framework/internal/tokenizer.js";
-import {parseComponentChild} from "../framework/internal/parser.js";
-import {applyComponentAttachments} from "../framework/internal/attacher.js";
+import {Serianilla} from "../framework/Serianilla.js";
 
-// const root = document.getElementById('root');
-// Serianilla.render(root, App);
-
-const {imports, template, attach} = App();
-const tokens = tokenize(template);
-console.log('tokens:', tokens);
-
-const ast = parseComponentChild(tokens);
-console.log('ast:', ast);
-
-const attachMap = attach ? new Map(Object.entries(attach)) : new Map();
-
-applyComponentAttachments(ast, attachMap, null);
-console.log('ast after attach:', ast);
+const root = document.getElementById('root');
+Serianilla.render(root, App);
