f8913db70d77b57ff1d655d82ba548d91be1c88a
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-19 00:17:13 +0300

    Edit transition function

--- src/utils/transition.js
+++ src/utils/transition.js
@@ -2,17 +2,17 @@ export function cssTransitionInOut(nodeRef, timeout, classes) {
     setTimeout(() => {
         nodeRef.current.classList.remove(classes + '_exit-active');
         nodeRef.current.classList.add(classes + '_enter');
-        setTimeout(() => {
-            nodeRef.current.classList.remove(classes + '_enter');
-            nodeRef.current.classList.add(classes + '_enter-active');
-            setTimeout(() => {
-                nodeRef.current.classList.remove(classes + '_enter-active');
-                nodeRef.current.classList.add(classes + '_exit');
-                setTimeout(() => {
-                    nodeRef.current.classList.remove(classes + '_exit');
-                    nodeRef.current.classList.add(classes + '_exit-active');
-                }, 60);
-            }, timeout);
-        }, 60);
     }, 0);
+    setTimeout(() => {
+        nodeRef.current.classList.remove(classes + '_enter');
+        nodeRef.current.classList.add(classes + '_enter-active');
+    }, 30);
+    setTimeout(() => {
+        nodeRef.current.classList.remove(classes + '_enter-active');
+        nodeRef.current.classList.add(classes + '_exit');
+    }, timeout + 30);
+    setTimeout(() => {
+        nodeRef.current.classList.remove(classes + '_exit');
+        nodeRef.current.classList.add(classes + '_exit-active');
+    }, timeout + 30);
 }
