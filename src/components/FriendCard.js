34801aeca26da6e4c4d3b20341e038daa8fd8839
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-21 07:37:45 +0300

    Edit cards

--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -5,9 +5,9 @@ export const FriendCard = (props) => {
     <div class="fri__card-container">
         <p class="fri__age-label">{props.age}</p>
         <img class="fri__card-image" src={props.photoURL} alt="" />
-        <h2>{props.firstName} {props.lastName}</h2>
+        <h2 class="fri__full-name">{props.firstName} {props.lastName}</h2>
         <p>{props.city}, {props.country}</p>
-        <p>{props.email}</p>
+        <p class="fri__email">{props.email}</p>
         <p>{props.phoneNumber}</p>
     </div>`;
 
--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,14 +1,15 @@
 import {FriendCard} from "./FriendCard.js";
+import {Loader} from "./ui/Loader.js";
 
-export const FriendsList = ({friends}) => {
-    const imports = [FriendCard];
+export const FriendsList = ({friends, isLoading}) => {
+    const imports = [FriendCard, Loader];
 
     const template = `
     <div class="fri__card-list">
         <$map items={friends} context="friend">
             <FriendCard
                 age={friend.age}
-                photoURL={friend.age}
+                photoURL={friend.photoURL}
                 firstName={friend.firstName}
                 lastName={friend.lastName}
                 city={friend.city}
@@ -16,10 +17,14 @@ export const FriendsList = ({friends}) => {
                 email={friend.email}
                 phoneNumber={friend.phoneNumber} />
         </$map>
+        <$if true={isLoading}>
+            <Loader/>            
+        </$if>
     </div>`;
 
     const attach = {
-        friends
+        friends,
+        isLoading,
     };
 
     return {imports, template, attach};
