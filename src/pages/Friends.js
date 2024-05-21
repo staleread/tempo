8761590b3eef80d61075c7d112bdf8dbc0ff7b30
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-21 07:42:44 +0300

    Edit friends page

--- src/pages/Friends.js
+++ src/pages/Friends.js
@@ -1,14 +1,52 @@
-export const Friends = ({locationContext}) => {
-    const imports = [];
+import {Serianilla} from "../../framework/Serianilla.js";
+import {fetchFriends, MOCK_fetchFriends} from "../http/FriendsAPI.js";
+import {FriendsList} from "../components/FriendsList.js";
+import {Button} from "../components/ui/Button.js";
+import {InputText} from "../components/ui/InputText.js";
+
+export const Friends = ({locationContext, notificationContext}) => {
+    const [friends, setFriends] = Serianilla.useState([]);
+    const [friendsChanged, setFriendsChanged] = Serianilla.useState(true);
+    const [isLoading, setIsLoading] = Serianilla.useState(false);
+
+    console.log(friends)
+
+    Serianilla.useEffect(async () => {
+        if (!friendsChanged) {
+            return;
+        }
+        setIsLoading(true);
+        try {
+            setFriends(await fetchFriends());
+            setFriendsChanged(false);
+        } catch (err) {
+            notificationContext.displayMessage('Error', err.message, 'error');
+        }
+        setIsLoading(false);
+    }, [friendsChanged]);
+
+    const imports = [FriendsList, Button, InputText];
 
     const template = `
-    <div>
-        <h1 style="color: white">Friends</h1>
-        <button onClick={goToLogin}>Login</button>
+    <div class="app__container">
+        <header class="fri__header">
+            <h1 class="fri__title">My Friends</h1>
+            <Button content="Log out" classes="fri__button"/>
+        </header>
+        <section class="fri__header">
+            <InputText placeholder="Search for friend" />
+            <div class="fri__action-buttons">
+                <Button content="Sort" classes="fri__button"/>
+                <Button content="Filter" classes="fri__button"/>            
+            </div>
+        </section>
+        <FriendsList friends={friends} isLoading={isLoading}/>
     </div>`;
 
     const attach = {
-        goToLogin: () => locationContext.setPathname('/login'),
+        friends,
+        isLoading,
+        rerender: () => setFriendsChanged(true),
     };
 
     return {imports, template, attach};
