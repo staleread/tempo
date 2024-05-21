1f9ea665701471f26f4b48fa0e7326da6b52d1e6
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-05-21 07:45:19 +0300

    Change fetched results count

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://randomuser.me/api/1.4/';
 export const API_SEED = 'unicorn';
-export const API_RESULTS_COUNT = 10;
+export const API_RESULTS_COUNT = 12;
 
 export const FRIENDS_ROUTE = '/friends';
 export const LOGIN_ROUTE = '/login';
