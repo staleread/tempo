824b344f66daef50a66f40e114dcc1812008f57e
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-06-24 09:10:38 +0300

    Remove everything but framework stuff

--- client/src/components/ui/Loader.js
+++
@@ -1,3 +0,0 @@
-export const Loader = () => {
-    return {template: `<div class="app__loader"></div>`};
-}
--- client/src/index.js
+++
@@ -1,5 +0,0 @@
-import {App} from "./App.js";
-import {Serianilla} from "../framework/Serianilla.js";
-
-const root = document.getElementById('root');
-Serianilla.render(root, App);
